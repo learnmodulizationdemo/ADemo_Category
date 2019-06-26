13788f7b2425e753989105140c43125398c0cce0
wjg <jinguow@foxmail.com>
2019-06-26 11:42:09 +0800

    Initial Commit

---
+++ ADemo_CategoryTests/ADemo_CategoryTests.m
@@ -0,0 +1,37 @@
+//
+//  ADemo_CategoryTests.m
+//  ADemo_CategoryTests
+//
+//  Created by wjg on 2019/6/26.
+//  Copyright © 2019 wjg. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ADemo_CategoryTests : XCTestCase
+
+@end
+
+@implementation ADemo_CategoryTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
